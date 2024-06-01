604095ee56b07c60c40af51d27ef5ae8862572b8
Murka <jalalovm@mail.ru>
2024-06-01 19:42:38 +0400

    Lesson1:Task4

---
+++ Desktop/portfolio/introduction to programming languages/Lesson1/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 15;
+int b = 7;
+int c =3;
+int d =13;
+int e = 4;
+
+int max = a;
+
+if (b>max)
+{
+    max= b;
+}
+if (c>max)
+{
+    max=c;
+}
+if (d>max)
+{
+    max=d;
+}
+if (e>max)
+{
+    max=e;
+}
+Console.WriteLine(max);
