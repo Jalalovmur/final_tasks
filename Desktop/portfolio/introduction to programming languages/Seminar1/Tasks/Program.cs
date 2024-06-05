542ee7f3a322ea9dd3d49b7ad0dd028057ec5620
Murka <jalalovm@mail.ru>
2024-06-05 20:27:30 +0400

    task1

---
+++ Desktop/portfolio/introduction to programming languages/Seminar1/Tasks/Program.cs
@@ -0,0 +1,15 @@
+﻿bool Multiple(int n)
+{
+    if (n % 7 == 0 && n % 23 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+System.Console.WriteLine ("Input number: " );
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine (Multiple(a));
