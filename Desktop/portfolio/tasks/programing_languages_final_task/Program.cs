f102b471533bcca6440b6b4054997b21703d53ed
Murka <jalalovm@mail.ru>
2024-07-02 20:45:49 +0400

    Second task saved

--- Desktop/portfolio/tasks/programing_languages_final_task/Program.cs
+++ Desktop/portfolio/tasks/programing_languages_final_task/Program.cs
@@ -23,24 +23,52 @@
 
 
 
+// // Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// // Даны два неотрицательных числа m и n.
+
+// int AckermannFunction(int m, int n)
+// {
+//     if (m == 0)
+//     {
+//         return n + 1;
+//     }
+//     else if (m > 0 && n == 0)
+//     {
+//         return AckermannFunction(m - 1, 1);
+//     }
+//     else
+//     {
+//         return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+//     }
+// }
+
+// int m = 3;
+// int n = 2;
+// Console.Write(AckermannFunction(m, n));
+
+
+
+
+
+
 // Задача 3: Задайте произвольный массив. 
 // Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
 
-void PrintElementsOfArray(int[] array, int i)
-{
-    if(i == 0)
-    {
-        Console.Write(array[i] + " ");
-    } 
-    else if(i > 0)
-    {
-        Console.Write(array[i] + " ");
-        PrintElementsOfArray(array, i - 1);
-    }
-}
+// void PrintElementsOfArray(int[] array, int i)
+// {
+//     if(i == 0)
+//     {
+//         Console.Write(array[i] + " ");
+//     } 
+//     else if(i > 0)
+//     {
+//         Console.Write(array[i] + " ");
+//         PrintElementsOfArray(array, i - 1);
+//     }
+// }
 
-int[] array = {1, 2, 3};
-int i = array.Length - 1;
-PrintElementsOfArray(array, i);
+// int[] array = {1, 2, 3};
+// int i = array.Length - 1;
+// PrintElementsOfArray(array, i);
 
 
