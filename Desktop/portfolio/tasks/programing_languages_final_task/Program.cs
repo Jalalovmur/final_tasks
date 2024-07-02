b42b1942c8b70b5278c91104cbbae3b8fdd5ca27
Murka <jalalovm@mail.ru>
2024-07-02 20:22:51 +0400

    third taks saved

--- Desktop/portfolio/tasks/programing_languages_final_task/Program.cs
+++ Desktop/portfolio/tasks/programing_languages_final_task/Program.cs
@@ -1,25 +1,46 @@
 ﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
 // Использовать рекурсию, не использовать циклы.
 
-void PrintNumbers(int m, int n)
+// void PrintNumbers(int m, int n)
+// {
+//     if (m == n)
+//     {
+//         Console.Write(n + " ");
+//     }
+//     if(m < n)
+//     {
+//         PrintNumbers(m, n - 1);
+//         Console.Write(n + " ");
+//     }
+// }
+
+// Console.WriteLine("Input first number: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// PrintNumbers(m ,n);
+
+
+
+
+// Задача 3: Задайте произвольный массив. 
+// Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+void PrintElementsOfArray(int[] array, int i)
 {
-    if (m == n)
+    if(i == 0)
     {
-        Console.Write(n + " ");
-    }
-    if(m < n)
+        Console.Write(array[i] + " ");
+    } 
+    else if(i > 0)
     {
-        PrintNumbers(m, n - 1);
-        Console.Write(n + " ");
+        Console.Write(array[i] + " ");
+        PrintElementsOfArray(array, i - 1);
     }
 }
 
-Console.WriteLine("Input first number: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input second number: ");
-int n = Convert.ToInt32(Console.ReadLine());
-PrintNumbers(m ,n);
-
-
+int[] array = {1, 2, 3};
+int i = array.Length - 1;
+PrintElementsOfArray(array, i);
 
 
